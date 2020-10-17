31d72a55dc9c25db07ce78f096a755cb49c328a7
Tsukiko <33276070+tsukikoko@users.noreply.github.com>
2020-10-18 00:11:19 +0900

    ✨ add Day 5 complete

---
+++ Practice/Tutorials/10_Days_of_Javascript/Day-5_ArrowFunctions.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim();
+  });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+  å
+  const answers = [];
+  for (let i = 0; i < nums.length; i++) {
+    (nums[i] % 2 === 0) ? answers.push(nums[i] * 2): answers.push(nums[i] * 3);
+  }
+  return answers;
+}
---
+++ Practice/Tutorials/10_Days_of_Javascript/Day-5_Template_Literals.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ *
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    // 配列を複数に分ける
+    const [x, y] = expressions;
+    // 数値の平方根を求める
+    const root = Math.sqrt((y*y)-(16*x))
+    const s1 = (y + root)/4;
+    const s2 = (y - root)/4;
+    // s2は常に小さくなる
+    return ([s2, s1]);
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+
+    [s1, s2] = [s1, s2].sort();
+
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
